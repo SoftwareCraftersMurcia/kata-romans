83d3e89f53407a1570b44cb2d01547bd12f153a0
JesusValera <jesus1902@outlook.com>
2021-04-30 18:55:18 +0200

    Refactor 6

--- php/src/RomanNumeralConverter.php
+++ php/src/RomanNumeralConverter.php
@@ -7,22 +7,24 @@ class RomanNumeralConverter
 {
     public function convertToRoman(int $arabicNumber): string
     {
-        if ($arabicNumber === 6) {
-            return 'VI';
-        }
-
-        if ($arabicNumber === 5) {
-            return 'V';
-        }
-
         $result = '';
+
+        if ($arabicNumber >= 5) {
+            $result .= 'V';
+            $arabicNumber -= 5;
+        }
+
+        if ($arabicNumber === 4) {
+            return 'IV';
+        }
+
         if ($arabicNumber > 3) {
             $arabicNumber -= 3;
             $result .= 'V';
         }
 
         for ($i = 0; $i < $arabicNumber; $i++) {
-            $result = 'I' . $result;
+            $result .= 'I';
         }
 
         return $result;
