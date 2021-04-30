26c90169afcab13b8b74dce860f76125f1776e1c
JesusValera <jesus1902@outlook.com>
2021-04-30 19:22:56 +0200

    Refactor converterToRoman method

--- php/src/RomanNumeralConverter.php
+++ php/src/RomanNumeralConverter.php
@@ -8,28 +8,22 @@ class RomanNumeralConverter
     public function convertToRoman(int $arabicNumber): string
     {
         $result = '';
-        while ($arabicNumber >= 10) {
-            $arabicNumber -= 10;
-            $result .= 'X';
-        }
 
-        if ($arabicNumber === 9) {
-            $arabicNumber -= 9;
-            $result .= 'IX';
-        }
+        $result .= $this->transform($arabicNumber, 10,'X');
+        $result .= $this->transform($arabicNumber, 9,'IX');
+        $result .= $this->transform($arabicNumber, 5,'V');
+        $result .= $this->transform($arabicNumber, 4,'IV');
+        $result .= $this->transform($arabicNumber, 1,'I');
 
-        if ($arabicNumber >= 5) {
-            $result .= 'V';
-            $arabicNumber -= 5;
-        }
+        return $result;
+    }
 
-        if ($arabicNumber === 4) {
-            $arabicNumber -= 4;
-            $result .= 'IV';
-        }
-
-        for ($i = 0; $i < $arabicNumber; $i++) {
-            $result .= 'I';
+    private function transform(int &$arabicNumber, int $number, string $symbol): string
+    {
+        $result = '';
+        while ($arabicNumber >= $number) {
+            $arabicNumber -= $number;
+            $result .= $symbol;
         }
 
         return $result;
